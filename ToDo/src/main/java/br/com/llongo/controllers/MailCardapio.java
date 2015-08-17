acd7ff44f417386b4f1c58bdb121db179ed9b15f
Diogo Longo <diogo.longo@tecnogroup.com.br>
2015-08-17 09:30:04 -0300

    alterações email e imagens

---
+++ ToDo/src/main/java/br/com/llongo/controllers/MailCardapio.java
@@ -0,0 +1,13 @@
+package br.com.llongo.controllers;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.springframework.stereotype.Component;
+@Component
+public class MailCardapio {
+	
+	public void name() {
+		VelocityEngine engine= new  VelocityEngine();
+		
+	}
+
+}
